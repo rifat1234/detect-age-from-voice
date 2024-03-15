3d651d5ca1fde419fe5ae3b3119728cb78a94cc1
Rifat Monzur <rifatmonzur@gmail.com>
2024-03-15 21:38:11 +0000

    ensemble learning model implemented

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import joblib
+import gdown
 
 # DESIGN implement changes to the standard streamlit UI/UX
 # --> optional, not relevant for the functionality of the component!
@@ -23,6 +24,13 @@ st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''',
 st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
             unsafe_allow_html=True)  # lightmode
 
+@st.cache_resource
+def load_rf_model():
+    url = 'https://drive.google.com/file/d/1jrJAIflrysoOMVW6mEWd40x2OKyhWy7D/view?usp=share_link'
+    output_path = 'rf_model.pkl'
+    gdown.download(url, output_path, quiet=False, fuzzy=True)
+    model = pickle.load(open('rf_model.pkl', 'rb'))
+    return model
 
 def audiorec_demo_app():
     # TITLE and Creator information
@@ -47,49 +55,65 @@ def audiorec_demo_app():
     # add some spacing and informative messages
     col_info, col_space = st.columns([0.57, 0.43])
     #with col_info:
+    with st.spinner("Processing... "):
+        if wav_audio_data is not None:
+            # display audio data as received on the Python side
+            col_playback, col_space = st.columns([0.58, 0.42])
+            with col_playback:
+                sfo = sf.SoundFile(io.BytesIO(wav_audio_data))
+                audio, sampling_rate = librosa.load(sfo)
 
-    if wav_audio_data is not None:
-        # display audio data as received on the Python side
-        col_playback, col_space = st.columns([0.58, 0.42])
-        with col_playback:
-            sfo = sf.SoundFile(io.BytesIO(wav_audio_data))
-            audio, sampling_rate = librosa.load(sfo)
+                features = list()
+                spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sampling_rate))
+                spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=audio, sr=sampling_rate))
+                spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(y=audio, sr=sampling_rate))
+                features.append(spectral_centroid)
+                features.append(spectral_bandwidth)
+                features.append(spectral_rolloff)
+                mfcc = librosa.feature.mfcc(y=audio, sr=sampling_rate, n_fft=2448)
+                for el in mfcc:
+                    features.append(np.mean(el))
 
-            features = list()
-            spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sampling_rate))
-            spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=audio, sr=sampling_rate))
-            spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(y=audio, sr=sampling_rate))
-            features.append(spectral_centroid)
-            features.append(spectral_bandwidth)
-            features.append(spectral_rolloff)
-            mfcc = librosa.feature.mfcc(y=audio, sr=sampling_rate, n_fft=2448)
-            for el in mfcc:
-                features.append(np.mean(el))
+                df = pd.DataFrame([features], columns=["spectral_centroid", "spectral_bandwidth", "spectral_rolloff",
+                                                       "mfcc1", "mfcc2", "mfcc3", "mfcc4", "mfcc5", "mfcc6", "mfcc7",
+                                                       "mfcc8",
+                                                       "mfcc9", "mfcc10", "mfcc11", "mfcc12", "mfcc13", "mfcc14", "mfcc15",
+                                                       "mfcc16",
+                                                       "mfcc17", "mfcc18", "mfcc19", "mfcc20"])
 
-            df = pd.DataFrame([features], columns=["spectral_centroid", "spectral_bandwidth", "spectral_rolloff",
-                                                   "mfcc1", "mfcc2", "mfcc3", "mfcc4", "mfcc5", "mfcc6", "mfcc7",
-                                                   "mfcc8",
-                                                   "mfcc9", "mfcc10", "mfcc11", "mfcc12", "mfcc13", "mfcc14", "mfcc15",
-                                                   "mfcc16",
-                                                   "mfcc17", "mfcc18", "mfcc19", "mfcc20"])
+                def scale_features(data):
+                    scaler = joblib.load('scaler.save')
+                    scaled_data = scaler.transform(np.array(data.iloc[:, :], dtype=float))
+                    # with data.iloc[:, 0:-1] we don't consider the label column
 
-            def scale_features(data):
-                scaler = joblib.load('scaler.save')
-                scaled_data = scaler.transform(np.array(data.iloc[:, :], dtype=float))
-                # with data.iloc[:, 0:-1] we don't consider the label column
+                    return scaled_data, scaler
 
-                return scaled_data, scaler
+                x, scaler = scale_features(df)
+                encoder = joblib.load('label_encoder.save')
 
-            x, scaler = scale_features(df)
-            encoder = joblib.load('label_encoder.save')
+                f_selector = joblib.load('feature_selector.save')
+                X_new = f_selector.transform(x)
 
-            f_selector = joblib.load('feature_selector.save')
-            X_new = f_selector.transform(x)
+                knn_model = pickle.load(open('knn_model.pkl', 'rb'))
+                knn_pred = knn_model.predict(X_new)
+                svc_model = pickle.load(open('svc_model.pkl', 'rb'))
+                svc_pred = svc_model.predict(X_new)
+                rf_model = load_rf_model()
+                rf_pred = rf_model.predict(X_new)
 
-            model = pickle.load(open('knn_model.pkl', 'rb'))
-            pred = model.predict(X_new)
-            output = f"You are in your  :green[**{encoder.inverse_transform(pred)[0]}**]"
-            st.markdown(output)
+                # Applying ensemble learning
+                knn_score = 0.886
+                svc_score = 0.866
+                rf_score = 0.691
+                w = [0] * 8
+
+                w[knn_pred[0]] += knn_score
+                w[svc_pred[0]] += svc_score
+                w[rf_pred[0]] += rf_score
+                pred = [np.argmax(w)]
+
+                output = f"You are in your  :green[**{encoder.inverse_transform(pred)[0]}**]"
+                st.markdown(output)
 
 
 if __name__ == '__main__':
